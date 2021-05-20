67d60d9c205c683dd9eca962103c9731c216562f
T-oga331 <81658428+T-oga331@users.noreply.github.com>
2021-05-20 18:14:39 +0900

    Initial commit

---
+++ Assets/UnityChan2D/Sprites/CubeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+
+    // キューブの移動速度
+    private float speed = -12;
+
+    //消滅位置
+    private float deadLine = -10;
+
+    // Use this initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        //画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        // デバッグ
+        Debug.Log("衝突時:" + other.gameObject.tag);
+
+        // 地面とキューブに当たったら音を鳴らす
+        if (other.gameObject.tag == "Untagged" || other.gameObject.tag == "CubeTag")
+        {
+            // 効果音
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+}
